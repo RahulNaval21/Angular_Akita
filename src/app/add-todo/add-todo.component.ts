48a57ddcfb58a1eb40c574f271ddd1e34d4bb284
RahulNaval21 <rahulnaval157821@gmail.com>
2025-09-27 02:22:34 +0530

    home component and add todo component changes

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from '@angular/material/button';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-todo',
   templateUrl: './add-todo.component.html',
-  styleUrl: './add-todo.component.scss'
+  standalone: true,
+  styleUrl: './add-todo.component.scss',
+  imports: [MatFormFieldModule, MatCardModule, MatInputModule, MatButtonModule, ReactiveFormsModule]
 })
-export class AddTodoComponent {
+export class AddTodoComponent implements OnInit {
+  constructor() { }
 
+  form: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null,[Validators.required]),
+      isCompleted: new FormControl(null,[Validators.required]),
+      userId: new FormControl(null,[Validators.required])
+    });
+  }
+
+  addTodo() {
+    console.log(this.form.value);
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatInputModule } from "@angular/material/input";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  imports: [MatCardModule, MatButtonModule, MatCardModule, MatProgressSpinnerModule, MatInputModule]
 })
 export class HomeComponent {
+  constructor(private router: Router) { }
 
+  addTodo() {
+    this.router.navigateByUrl('/add-todo');
+  }
 }
